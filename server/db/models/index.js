5b69c4de14f07d48c4fb8e09e348af8bd9e7658b
Heine574 <johannes@niemela.se>
2022-04-19 14:26:55 -0500

    added User_Conversation through table

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,12 +2,12 @@ const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
 const Message_Reader = require("./message_reader");
+const User_Conversation = require("./user_conversation");
 
 // associations
 
 User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+Conversation.belongsToMany(User, { through: User_Conversation });
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
 Message.belongsToMany(User, { through: Message_Reader });
--- server/db/seed.js
+++ server/db/seed.js
@@ -23,10 +23,9 @@ async function seed() {
       "https://res.cloudinary.com/dmlvthmqr/image/upload/v1607914466/messenger/775db5e79c5294846949f1f55059b53317f51e30_s3back.png",
   });
 
-  const santaigoConvo = await Conversation.create({
-    user1Id: thomas.id,
-    user2Id: santiago.id,
-  });
+  const santaigoConvo = await Conversation.create({});
+  santaigoConvo.addUser(thomas);
+  santaigoConvo.addUser(santiago);
 
   await Message.create({
     conversationId: santaigoConvo.id,
@@ -51,10 +50,10 @@ async function seed() {
     photoUrl:
       "https://res.cloudinary.com/dmlvthmqr/image/upload/v1607914468/messenger/8bc2e13b8ab74765fd57f0880f318eed1c3fb001_fownwt.png",
   });
-  const chiumboConvo = await Conversation.create({
-    user1Id: chiumbo.id,
-    user2Id: thomas.id,
-  });
+  const chiumboConvo = await Conversation.create({});
+  chiumboConvo.addUser(chiumbo);
+  chiumboConvo.addUser(thomas);
+
   await Message.create({
     conversationId: chiumboConvo.id,
     senderId: chiumbo.id,
@@ -68,10 +67,9 @@ async function seed() {
     photoUrl:
       "https://res.cloudinary.com/dmlvthmqr/image/upload/v1607914466/messenger/6c4faa7d65bc24221c3d369a8889928158daede4_vk5tyg.png",
   });
-  const hualingConvo = await Conversation.create({
-    user2Id: hualing.id,
-    user1Id: thomas.id,
-  });
+  const hualingConvo = await Conversation.create({});
+  hualingConvo.addUser(thomas);
+  hualingConvo.addUser(hualing);
 
   for (let i = 0; i < 11; i++) {
     await Message.create({
